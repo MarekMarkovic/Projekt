b67b3d647d10a475bfbcc2fbe398d406a23eddd1
Marek Markovic <marekmarkovic@gmail.com>
2023-03-01 14:44:48 +0100

    Skuska

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+
+
+
+    public static void main(String[] args) {
+        int i = 5;
+        int k = 1;
+        for (int j = 0; j < i; j++) {
+            System.out.println("-");
+
+            for (int l = 1 + j; l < i; l++){
+                System.out.println("x");
+            }
+
+            for ()
+        }
+    }
+
+}
